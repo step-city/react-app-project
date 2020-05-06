45b240761e6cb8acd45b6f5b6dcdbdb6eadd3586
guonn <guonn@sit.com.cn>
2020-05-06 16:15:40 +0800

    add mock datas

--- src/pages/modules/myProducts/child/myProdutLists.js
+++ src/pages/modules/myProducts/child/myProdutLists.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import iceboxImg from '../../../../assets/img/face.jpg'
+import axios from 'axios'
 
 class productLists extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
 
         this.state = {
             My_pro_arr: '',
+            dataSource: ''
         }
     }
     //生命周期:挂载成功后给state赋值
@@ -17,18 +19,31 @@ class productLists extends React.Component {
         this.setState({
             My_pro_arr: Add_pro_arr
         })
+        this.fetchData()
+    }
+    //获取Mock数据
+    fetchData() {
+        const that = this;
+        axios.get('http://localhost:8090/mode2/DataOne').then((res) => {
+            if (res.status == 200) {
+                console.log('获取Mock数据 :>> ', res.data);
+                that.setState({
+                    dataSource: res.data.dataSource
+                })
+            }
+        })
     }
 
-      //删除产品函数
-      deleteAddPro = (e) =>{
+    //删除产品函数
+    deleteAddPro = (e) => {
         let idx = e.target.getAttribute('data-idx');
         // 删除点击的产品 并重新渲染DOM
-        this.state.My_pro_arr.splice(idx,1);
+        this.state.My_pro_arr.splice(idx, 1);
         console.log(this.state.My_pro_arr);
         //更新状态
         this.setState({
-            My_pro_arr:this.state.My_pro_arr
-        },function(){
+            My_pro_arr: this.state.My_pro_arr
+        }, function () {
             console.log('setState更新成功')
             this.jugleContent()
         })
@@ -78,7 +93,7 @@ class productLists extends React.Component {
 const mapStateToProps = (state) => {
     console.log(state);
     return {
-        Add_pro_arr:state.pro_Info
+        Add_pro_arr: state.pro_Info
     }
 }
 
